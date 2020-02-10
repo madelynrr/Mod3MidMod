3ca5249da83d8780c26a9035caa8792c29c01bfc
Madelyn Romero <m.romero013@gmail.com>
2020-02-10 10:31:06 -0700

    Begin declarative driving for abstraction

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,25 +1,25 @@
 class SearchController < ApplicationController
 
   def index
-    house = params[:house]
-
-    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
-      faraday.params = {key: ENV['HARRY_POTTER_KEY'],
-                        house: "#{house}",
-                        orderOfThePhoenix: true}
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = conn.get("characters")
-
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    @members = json.map do |member|
-      Character.new(member)
-    end
-
-
-
+    render locals: {
+      search_results: OrderByHouseSearch.new(params[:house])
+    }
+    # house = params[:house]
+    #
+    # conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
+    #   faraday.params = {key: ENV['HARRY_POTTER_KEY'],
+    #                     house: "#{house}",
+    #                     orderOfThePhoenix: true}
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    #
+    # response = conn.get("characters")
+    #
+    # json = JSON.parse(response.body, symbolize_names: true)
+    #
+    # @members = json.map do |member|
+    #   Character.new(member)
+    # end
   end
 
 end
