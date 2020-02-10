c857dec9138e3d8d7f9afcd093c9bb298c3fc768
Madelyn Romero <m.romero013@gmail.com>
2020-02-10 10:48:16 -0700

    Create test to move API call out to a service

---
+++ spec/services/potter_service_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe PotterService do
+  context "instance methods" do
+    context "#members_by_house" do
+      it "returns the member data" do
+        search = subject.members_by_house("Gryffindor")
+        expect(search).to be_a Hash
+        expect(search[:results]).to be_an Array
+        expect(search[:results].count).to eq 21
+        member = search[:results].first
+
+        expect(member).to have_key :name
+        expect(member).to have_key :house        
+      end
+    end
+  end
+end
