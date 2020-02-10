0c6790f26295a8d1a3c2d3bc90e079d0e26f97cd
Madelyn Romero <m.romero013@gmail.com>
2020-02-10 10:13:57 -0700

    Create test for character PORO

---
+++ spec/POROs/characters_poro_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "when a character is created" do
+  it "it has attributes" do
+    attributes = {
+      name: "Sirius Black",
+      role: "Godfather",
+      house: "Gryffindor",
+      patronus: "X"
+    }
+
+    character = Character.new(attributes)
+
+    expect(character).to be_a Character
+    expect(character.name). to eq("Sirius Black")
+    expect(character.role). to eq("Godfather")
+    expect(character.house). to eq("Gryffindor")
+    expect(character.patronus). to eq("X")
+  end
+
+end
