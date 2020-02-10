6eb37969bf356b49fc187a7ef33f7109ca19ab77
Madelyn Romero <m.romero013@gmail.com>
2020-02-10 10:15:51 -0700

    Add test to return n/a if attribute not in given attributes

--- spec/POROs/characters_poro_spec.rb
+++ spec/POROs/characters_poro_spec.rb
@@ -18,4 +18,21 @@ describe "when a character is created" do
     expect(character.patronus). to eq("X")
   end
 
+  it "returns n/a if attribute not included" do
+    attributes = {
+      name: "Sirius Black",
+      house: "Gryffindor",
+      patronus: "X"
+    }
+
+    character = Character.new(attributes)
+
+    expect(character).to be_a Character
+    expect(character.name). to eq("Sirius Black")
+    expect(character.role). to eq("n/a")
+    expect(character.house). to eq("Gryffindor")
+    expect(character.patronus). to eq("X")
+
+  end
+
 end
