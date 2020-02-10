b08c1726f0d6e19deacd9eb14f7275a4ef5787e9
Madelyn Romero <m.romero013@gmail.com>
2020-02-10 10:36:51 -0700

    Place Faraday connection and results in order by house search PORO

--- app/POROs/order_by_house_search.rb
+++ app/POROs/order_by_house_search.rb
@@ -4,4 +4,25 @@ class OrderByHouseSearch
     @house = house_attribute
   end
 
+  def member_count
+    members.count
+  end
+
+  def members
+    conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
+      faraday.params = {key: ENV['HARRY_POTTER_KEY'],
+                        house: @house,
+                        orderOfThePhoenix: true}
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("characters")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    @members = json.map do |member|
+      Character.new(member)
+    end
+  end
+
 end
