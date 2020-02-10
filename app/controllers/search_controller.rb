49dcb3071eaa93e2c7032e659a525d466cc0a70d
Madelyn Romero <m.romero013@gmail.com>
2020-02-10 10:36:28 -0700

    Take Faraday logic out of controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,20 +6,7 @@ class SearchController < ApplicationController
     }
     # house = params[:house]
     #
-    # conn = Faraday.new(url: "https://www.potterapi.com/v1/") do |faraday|
-    #   faraday.params = {key: ENV['HARRY_POTTER_KEY'],
-    #                     house: "#{house}",
-    #                     orderOfThePhoenix: true}
-    #   faraday.adapter Faraday.default_adapter
-    # end
-    #
-    # response = conn.get("characters")
-    #
-    # json = JSON.parse(response.body, symbolize_names: true)
-    #
-    # @members = json.map do |member|
-    #   Character.new(member)
-    # end
+    
   end
 
 end
